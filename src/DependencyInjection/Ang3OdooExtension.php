ee46abf940ef69bd10bfde82d280f79fdf639906
Joanis Rouanet <joanis.ang3@gmail.com>
2020-07-07 22:37:50 +0200

    Added missing ORM default connection argument in extension

--- src/DependencyInjection/Ang3OdooExtension.php
+++ src/DependencyInjection/Ang3OdooExtension.php
@@ -83,11 +83,11 @@ class Ang3OdooExtension extends Extension
         $ormEnabled = $ormConfig['enabled'] ?? false;
 
         if ($ormEnabled) {
-            $this->loadOdooOrm($container, $connections, $ormConfig);
+            $this->loadOdooOrm($container, $connections, $config['default_connection'], $ormConfig);
         }
     }
 
-    public function loadOdooOrm(ContainerBuilder $container, array $connections, array $config): void
+    public function loadOdooOrm(ContainerBuilder $container, array $connections, string $defaultConnection, array $config): void
     {
         $managers = $config['managers'] ?? [];
         $objectManagerRegistry = $container->getDefinition(ObjectManagerRegistry::class);
